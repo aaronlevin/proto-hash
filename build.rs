ace1fce3e842b7e8d5d5ed66aa123b467e084973
aaronlevin <vilevin@gmail.com>
2018-05-19 12:38:59 -0400

    Initial commit

---
+++ build.rs
@@ -0,0 +1,13 @@
+extern crate protoc_rust;
+
+fn main() {
+
+        protoc_rust::run(protoc_rust::Args {
+        out_dir: "src",
+        input: &["protos/descriptors.proto"],
+        includes: &["protos"],
+        customize: protoc_rust::Customize {
+            ..Default::default()
+        },
+    }).expect("protoc");
+}
